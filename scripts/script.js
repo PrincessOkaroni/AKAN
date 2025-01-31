3209470b1b41a3c2476f83f9a1554232473a1d23
Nekesa Purity <nekesapurity20@gmail.com>
2025-01-31 20:40:36 +0300

    styled the body

---
+++ scripts/script.js
@@ -0,0 +1,44 @@
+document
+  .getElementById("AkanForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const day = parseInt(document.getElementById("Day").value);
+    const month = parseInt(document.getElementById("Month").value);
+    const year = parseInt(document.getElementById("year").value);
+    const gender = document.querySelector('input[name="Male"]:checked')
+      ? "male"
+      : "female";
+
+    const date = new Date(year, month - 1, day);
+    const dayOfWeek = date.getDay();
+
+    const maleNames = [
+      "Kwasi",
+      "Kwadwo",
+      "Kwabena",
+      "Kwaku",
+      "Yaw",
+      "Kofi",
+      "Kwame",
+    ];
+    const femaleNames = [
+      "Akosua",
+      "Adwoa",
+      "Abenaa",
+      "Akua",
+      "Yaa",
+      "Afua",
+      "Ama",
+    ];
+
+    let akanName = "";
+
+    if (gender === "male") {
+      akanName = maleNames[dayOfWeek];
+    } else if (gender === "female") {
+      akanName = femaleNames[dayOfWeek];
+    }
+
+    alert("Your Akan name is: " + akanName);
+  });
